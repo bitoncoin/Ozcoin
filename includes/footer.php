a1cdc5d58cdaa21824e0d631742772c043e8c609
Wayno <locos@westnet.com.au>
2011-07-02 08:33:07 +0800

    Changes

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-			</div>
-			<div class"clear"></div>
-			<div id="footer">
-			<b>Ozco.in</b> - Donations: 1Gzcbs8dDYzf16qFWKHc5kWKuH8nji3pVt
-			</div>			
-		</div>
-	</body>
-</html>
--- mt.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mtgox = exec('curl https://mtgox.com/code/data/ticker.php');
-echo "$mtgox";
-?>
